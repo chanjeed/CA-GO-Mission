de9a931f033c10f7c9ed2753da503a1133141ad6
Chanya Kukulprasong <chanjeedtu75@gmail.com>
2021-02-16 23:38:16 +0900

    add gacha mysql

--- initdb.d/init.sql
+++ initdb.d/init.sql
@@ -8,4 +8,42 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name TEXT NOT NULL,
 token TEXT NOT NULL
 )DEFAULT CHARACTER SET=utf8;
+
+
+DROP TABLE IF EXISTS Characters;
+  
+CREATE TABLE Characters (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name TEXT NOT NULL
+)DEFAULT CHARACTER SET=utf8;
+
+INSERT INTO Characters (name) VALUES ("Totoro");
+INSERT INTO Characters (name) VALUES ("Chihiro");
+INSERT INTO Characters (name) VALUES ("Kiki");
+INSERT INTO Characters (name) VALUES ("Marnie");
+
+
+DROP TABLE IF EXISTS UserCharacters; 
+
+CREATE TABLE UserCharacters (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+userId INT NOT NULL,
+characterId INT NOT NULL,
+ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)DEFAULT CHARACTER SET=utf8;
  
+
+
+DROP TABLE IF EXISTS Gachas;
+  
+CREATE TABLE Gachas (
+characterId INT NOT NULL PRIMARY KEY,
+number INT NOT NULL
+)DEFAULT CHARACTER SET=utf8;
+
+INSERT INTO Gachas (characterId,number) VALUES (1,10);
+INSERT INTO Gachas (characterId,number) VALUES (2,10);
+INSERT INTO Gachas (characterId,number) VALUES (3,10);
+INSERT INTO Gachas (characterId,number) VALUES (4,10);
+ 
+
